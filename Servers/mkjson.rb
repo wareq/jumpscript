96e5a346a1c0cb4c0fe06245ebef68748684dd68
Koaps <koaps@yahoo.com>
2013-06-16 00:35:34 -0600

    Update mkjson.rb

--- Servers/mkjson.rb
+++ Servers/mkjson.rb
@@ -2,6 +2,7 @@
 
 =begin
 Copyright 2013 Koaps / Nullworks.
+http://nullworks.wordpress.com
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
